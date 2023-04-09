78ad998da3904ba9b8a0a249b06dcfc1e622fe33
xy113 <songdewei@163.com>
2023-04-09 23:55:19 +0800

    1.1.0

--- index.js
+++ index.js
@@ -1,22 +1,78 @@
 import React from "react";
-import {requireNativeComponent,StyleSheet} from "react-native";
+import {Animated, requireNativeComponent, StyleSheet, View, Dimensions, Image} from "react-native";
+
+const lineWidth = Dimensions.get("window").width
+const startY = Dimensions.get("window").height * 0.1;
+const endY = Dimensions.get("window").height * 0.9;
 
 export default class BarcodeView extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            top: new Animated.Value(startY),
+            opacity: 1
+        }
     }
 
+    startAnimate = () => {
+        this.animate = Animated.timing(this.state.top, {
+            toValue: endY,
+            duration: 3000,
+            useNativeDriver: false
+        });
+        this.animate.start(({finished}) => {
+            if (finished) {
+                this.setState({top: new Animated.Value(startY), opacity: 1}, () => {
+                    this.startAnimate();
+                });
+            }
+        });
+    }
+
+    stopAnimate = () => {
+        this.animate.stop();
+        this.setState({opacity: 0});
+    }
+
+    componentDidMount() {
+        this.startAnimate();
+    }
 
     render() {
-        let {onRead,...props} = this.props;
-        return(
-            <RNBarcodeView
-                style={StyleSheet.absoluteFill}
-                onBarcodeRead={e=>{
-                    onRead && onRead(e)
-                }}
-                {...props}
-            />
+        let {onRead, ...props} = this.props;
+        return (
+            <View style={StyleSheet.absoluteFill}>
+                <RNBarcodeView
+                    style={{flex: 1}}
+                    onBarcodeRead={e => {
+                        this.stopAnimate();
+                        onRead && onRead(e)
+                    }}
+                    {...props}
+                />
+                <Animated.View
+                    style={{
+                        top: this.state.top,
+                        opacity: this.state.opacity,
+                        position: 'absolute',
+                        left: 0,
+                        right: 0,
+                        alignItems: 'center',
+                        justifyContent: 'center'
+                    }}
+                >
+                    <Image
+                        source={require('./line.png')}
+                        style={{
+                            width: lineWidth,
+                            height: 50,
+                            tintColor: '#06D96B',
+                            resizeMode: 'contain'
+                        }}
+                    />
+                </Animated.View>
+                {this.props.children}
+            </View>
         )
     }
 }
